6603d28da69ab9802eab247d6203e0b0f33de975
暾暾 <631078133@qq.com>
2018-06-07 17:24:04 +0800

    base framework

---
+++ pinhole/run.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+class main(object):
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+from os.path import join, isfile
+from os import walk
+from pinhole import __version__, __author__
+
+
+def read_file(filename):
+    with open(filename) as fp:
+        return fp.read().strip()
+
+
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if not line.startswith('#')]
+
+
+def package_files(directories):
+    paths = []
+    for item in directories:
+        if isfile(item):
+            paths.append(join('..', item))
+            continue
+        for (path, directories, filenames) in walk(item):
+            for filename in filenames:
+                paths.append(join('..', path, filename))
+    return paths
+
+
+setup(
+    name='Pinhole',
+    version=__version__,
+    description='Less Is More.',
+    long_description=
+    'Go to https://github.com/uid00000000/pinhole for more information.',
+    author=__author__,
+    author_email='markbrownjayb@qq.com',
+    url='https://github.com/uid00000000/pinhole',
+    license='MIT',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+
+    entry_points={
+        'console_scripts': {
+            'pinhole = pinhole.run:main'
+        },
+    },
+
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
